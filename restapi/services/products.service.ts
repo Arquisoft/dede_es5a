1b2987824a485c97fc22f9bc61b9b46ce03a9f65
Diego <uo276406@uniovi.es>
2022-03-03 23:49:08 +0100

    Añadidos comentarios en el código

--- restapi/services/products.service.ts
+++ restapi/services/products.service.ts
@@ -8,18 +8,18 @@ dotenv.config();
 export const collections: { products?: mongoDB.Collection } = {} //Datos de la base de datos
 
 const URI: string = process.env.DATABASE_URI != undefined ? process.env.DATABASE_URI : "undefined"; //Datos para la conexión
-const COLLECTION_NAME: string = process.env.COLLECTION_PRODUCTS != undefined ? process.env.COLLECTION_PRODUCTS : "undefined";
+const COLLECTION_PRODUCTS: string = process.env.COLLECTION_PRODUCTS != undefined ? process.env.COLLECTION_PRODUCTS : "undefined";
 
 // Initialize Connection
-export async function connectToDatabase() {
+export async function connectToProductsDatabase() {
 
     const client: mongoDB.MongoClient = new mongoDB.MongoClient(URI); //Obtenemos el cliente con la URI
     await client.connect(); //Nos conectamos
 
     const db: mongoDB.Db = client.db(process.env.DB_NAME); //Vamos a la base de datos específica
-    const productsCollection: mongoDB.Collection = db.collection(COLLECTION_NAME); //vamos a la conexión concreta
+    const productsCollection: mongoDB.Collection = db.collection(COLLECTION_PRODUCTS); //vamos a la colección de productos en mongoDB
 
-    collections.products = productsCollection;
+    collections.products = productsCollection; //Almacena la colección en la variable products de co
 
-    console.log(`Successfully connected to database: ${db.databaseName} and collection: ${productsCollection.collectionName}`);
+    console.log(`Successfully connected to (database: ${db.databaseName} - collection: ${productsCollection.collectionName})`);
 }
