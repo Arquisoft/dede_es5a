f21fcb4abb986691ad24ec00bbc27f1b756b7997
mariaurrutia97 <uo251282@uniovi.es>
2022-03-27 20:27:49 +0200

    Inicio de sesión con SOLID

--- webapp/src/components/User/UserLogIn.tsx
+++ webapp/src/components/User/UserLogIn.tsx
@@ -1,72 +1,43 @@
-import * as React from 'react'
+import { useState, useEffect } from "react";
+import { LoginButton } from "@inrupt/solid-ui-react";
+import { Button, TextField, FormGroup, Container, Box, Avatar } from "@mui/material";
+import React from "react";
 
-import { useForm } from 'react-hook-form'
-import { login } from '../../api/api'
-import { useNavigate } from 'react-router-dom'
-import { Container, Box, Avatar, TextField, Button } from '@mui/material'
-
-interface SignInPops {
-  setPage: (page: string) => void
-}
-
-export default function UserLogIn(): JSX.Element {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm()
-  const navigate = useNavigate()
-
-  async function doLogin(data: any) {
-    const logued = await login(data)
-    if (logued) navigate('/products', { replace: true })
-  }
+const LoginForm = () => {
+  const [idp, setIdp] = useState("https://broker.pod.inrupt.com");
 
   return (
-    <React.Fragment>
-      <Container component="main" maxWidth="xs">
-        <Box
-          sx={{
-            marginTop: 10,
-            alignItems: 'center',
-            flexDirection: 'column',
-            display: 'inline-flex',
-          }}
-        >
-          <Box>
-            <Avatar src="/images/sign-in.png" />
-            <h2>WELCOME</h2>
-            <form onSubmit={handleSubmit(doLogin)}>
-              <TextField
-                id="email"
-                label="Email"
-                variant="standard"
-                type="text"
-                margin="dense"
-                size="small"
-                required
-                fullWidth
-                {...register('email', { required: true })}
-              />
+      <React.Fragment>
+          <Container component="main" maxWidth="xs">
+              <Box
+                  sx={{
+                      marginTop: 10,
+                      alignItems: 'center',
+                      flexDirection: 'column',
+                      display: 'inline-flex',
+                  }}
+              >
+                <Avatar src="/images/sign-in.png" />
+                <h2>WELCOME</h2>
+                <TextField
+                    label="Identity Provider"
+                    placeholder="Identity Provider"
+                    style={{width: '250px'}}
+                    type="url"
+                    value={idp}
+                    color="primary"
+                    onChange={(e) => setIdp(e.target.value)}
+                />
+                <LoginButton oidcIssuer={idp} redirectUrl="http://localhost:3000">
+                    <Button variant="contained" type="submit" sx={{ mt: 4 }}>
+                        Sign In
+                    </Button>
+                </LoginButton>
 
-              <TextField
-                id="password"
-                label="Password"
-                variant="standard"
-                type="password"
-                size="small"
-                margin="dense"
-                required
-                fullWidth
-                {...register('password', { required: true })}
-              />
-              <Button variant="contained" type="submit" sx={{ mt: 4 }}>
-                Sign In
-              </Button>
-            </form>
-          </Box>
-        </Box>
-      </Container>
-    </React.Fragment>
-  )
+              </Box>
+          </Container>
+      </React.Fragment>
+  );
 }
+
+export default LoginForm;
