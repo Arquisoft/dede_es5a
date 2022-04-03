ee27bfc2ed122c1693e9f2b6fb01e99e2c83726c
SofiaYYQ <xiatiansofi@gmail.com>
2022-04-03 19:41:54 +0200

    Create ShoppingCartDrawer.tsx

---
+++ webapp/src/components/Cart/ShoppingCartDrawer.tsx
@@ -0,0 +1,70 @@
+import {
+  Stack,
+  Divider,
+  Box,
+  Tooltip,
+  Badge,
+  IconButton,
+  Drawer,
+  Container,
+  Typography,
+  Button,
+  styled,
+} from '@mui/material'
+import React, { useContext } from 'react'
+import { CartContext } from '../../contexts/CartContext'
+import calculateCartTotal from '../../helpers/calculateCartTotal'
+import calculateTotalQuantity from '../../helpers/calculateTotalQuantity'
+import CartItem from './CartItem'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import { useNavigate } from 'react-router-dom'
+import ShoppingCart from './ShoppingCart'
+
+const StyledBadge = styled(Badge)({
+  "& .MuiBadge-badge": {
+    color: "black",
+    backgroundColor: "#f29f05"
+  }
+});
+
+
+
+export default function ShoppingCartDrawer() {
+  const { cartProducts } = useContext(CartContext)
+  const [state, setState] = React.useState(false)
+  const navigate = useNavigate()
+  const toggleDrawer = (open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent,
+  ) => {
+    setState(open)
+  }
+
+  return (
+    <Box sx={{ flexGrow: 0 }}>
+      <Tooltip title="Open cart">
+        <StyledBadge
+          badgeContent={calculateTotalQuantity(cartProducts)}
+        >
+          <IconButton onClick={toggleDrawer(true)} sx={{ p: 0 }}>
+            <ShoppingCartIcon fontSize="large" style={{ color: 'white' }}></ShoppingCartIcon>
+          </IconButton>
+        </StyledBadge>
+      </Tooltip>
+      <Drawer anchor={'right'} open={state} onClose={toggleDrawer(false)}>
+        <Container>
+        <ShoppingCart />
+        
+            <Button
+              disabled={cartProducts.length === 0}
+              fullWidth
+              variant="contained"
+              onClick={() => navigate('/saleprocess')}
+            >
+              Process Order
+            </Button>
+        </Container>
+      </Drawer>
+    </Box>
+    
+  )
+}
