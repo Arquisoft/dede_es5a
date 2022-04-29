8ddbb1f7536c5e8f9b72efdc6ca9826da3ac711d
mariaurrutia97 <uo251282@uniovi.es>
2022-04-29 15:51:45 +0200

    Pruebas unitarias userAddress

--- webapp/src/components/User/UserLogIn.tsx
+++ webapp/src/components/User/UserLogIn.tsx
@@ -9,6 +9,7 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  Typography,
 } from '@mui/material'
 import { env } from 'process'
 
@@ -37,8 +38,9 @@ const LoginForm = () => {
               id="demo-simple-select"
               style={{ width: '270px' }}
               value={idp}
-              label="Age"
+              label="Provider"
               onChange={(e) => setIdp(e.target.value)}
+              data-testid="provider"
             >
               <MenuItem
                 onChange={() => setIdp('https://broker.pod.inrupt.com')}
---
+++ webapp/src/helpers/userAddress.test.tsx
@@ -0,0 +1,37 @@
+import GetUserAddress from "../helpers/userAddress";
+import * as solid from "@inrupt/solid-client";
+
+jest.mock('@inrupt/solid-client');
+
+test('check that adress is generated correctly', async () => {
+
+
+  jest.spyOn(solid, 'getUrlAll').mockImplementation((profile, type) => ['address1', 'address2']);
+  jest.spyOn(solid, 'getStringNoLocale').mockImplementation((profile, type) => 'field');
+
+  // Act
+  const address = await GetUserAddress('');
+
+  // Assert
+  for (let i = 0; i < address.length; i++) {
+    expect(address[i][0]).toBe('field');
+    expect(address[i][1]).toBe('field');
+    expect(address[i][2]).toBe('field');
+    expect(address[i][3]).toBe('field');
+    expect(address[i][4]).toBe('field');
+  }
+});
+
+test('check that adress is generated correctly when there isn\'t one', async () => {
+
+
+  jest.spyOn(solid, 'getUrlAll').mockImplementation((profile, type) => []);
+  jest.spyOn(solid, 'getStringNoLocale').mockImplementation((profile, type) => 'field');
+
+  // Act
+  const address = await GetUserAddress('');
+
+  // Assert
+    expect(address.length).toBe(0);
+  
+});
