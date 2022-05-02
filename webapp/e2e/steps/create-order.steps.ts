f0432afa001ea8f9a53673cf79996e7b8fe91c98
Diego González Suárez <UO276406@uniovi.es>
2022-05-02 21:51:34 +0200

    Test creación de pedido... No funciona. No sé muy bien por qué... Hay que intentar revisarlo.

--- webapp/e2e/steps/create-order.steps.ts
+++ webapp/e2e/steps/create-order.steps.ts
@@ -1,7 +1,7 @@
 import { defineFeature, loadFeature } from 'jest-cucumber';
 import puppeteer from "puppeteer";
 
-const feature = loadFeature('./features/add-to-cart.feature');
+const feature = loadFeature('./features/create-order.feature');
 
 let page: puppeteer.Page;
 let browser: puppeteer.Browser;
@@ -22,25 +22,44 @@ defineFeature(feature, test => {
       .catch(() => {});
   });
 
-  test('User adds a product to the cart', ({given,when,then}) => {
-
-    given('A user who founds the site', () => {
-      console.log('Checking product added...')
+  test('User adds a product to the cart and decide to buy it', ({given,when,then}) => {
+    given('A user adds a product', async () => {
+      console.log('Checking order creation...')
     });
 
-    when('He selects the product\'s size, he decides to add it to the cart', async () => {
+    when('He goes to the cart and start procesing the order', async () => {
       //Se selecciona la talla en el combobox
       await expect(page).toClick('div.MuiGrid-grid-xs-4:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1)')
       await expect(page).toClick('#menu- > div:nth-child(3) > ul:nth-child(1) > li:nth-child(1)')
 
       //Se hace click para añadir al carrito
       await expect(page).toClick('div.MuiGrid-grid-xs-4:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > button:nth-child(2)')
+    
+
+      //Se va al carrito
+      await expect(page).toClick('.MuiBadge-root > button:nth-child(1)')
+
+      //Se clica en "process order"
+      await expect(page).toClick('.MuiButton-fullWidth')
+
+      //Se clica en "next"
+      await expect(page).toClick("button.css-1e6y48t-MuiButtonBase-root-MuiButton-root:nth-child(3)")
+
+      //Se selcciona una dirección
+      await expect(page).toClick("div.MuiDataGrid-row:nth-child(1)")
+
+      //Se clica en "next"
+      await expect(page).toClick("button.css-1e6y48t-MuiButtonBase-root-MuiButton-root:nth-child(3)")
+
+      //Se hace clcick en pay y se compruea que se está en la página en cuestión
+      await expect(page).toMatch("Simulating the filling of payment data")
+      await expect(page).toClick(".css-1e6y48t-MuiButtonBase-root-MuiButton-root")
+
     });
 
-    then('A confirmation message should be shown in the screen and the product is added', async () => {
-      //Se muestra añadido
-      await expect(page).toClick('.MuiBadge-root > button:nth-child(1)')
-      await expect(page).toMatch('British')
+    then('Order is created', async () => {
+      await expect(page).toMatch("The sale has been done and it can proceed with the delivery.")
+
     });
   })
 
