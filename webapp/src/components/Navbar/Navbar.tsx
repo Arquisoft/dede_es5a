f7eb18bd13367effe178bae73b9bda0c9769e248
mariaurrutia97 <uo251282@uniovi.es>
2022-05-03 17:08:20 +0200

    Quitadas categorías del nav

--- webapp/src/components/Navbar/Navbar.tsx
+++ webapp/src/components/Navbar/Navbar.tsx
@@ -19,12 +19,8 @@ import {
   Alert,
 } from '@mui/material'
 
-import MenuIcon from '@mui/icons-material/Menu'
-
 import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
-
-const pages = ['Women', 'Men', 'Kids']
 
 const NavBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
@@ -55,6 +51,7 @@ const NavBar = () => {
   const handleLogout = () => {
     logout()
     handleCloseUserMenu()
+    navigate('/home')
     setMessage('Sesión cerrada satisfactoriamente')
   }
 
@@ -100,41 +97,7 @@ const NavBar = () => {
                 />
               </IconButton>
             </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: 'block', md: 'none' },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
+            <Box sx={{ flexGrow: 1}}> 
             </Box>
             <Typography
               variant="h6"
@@ -158,16 +121,7 @@ const NavBar = () => {
                 />
               </IconButton>
             </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  {page}
-                </Button>
-              ))}
+            <Box sx={{ flexGrow: 1}}> 
             </Box>
 
           <Box sx={{ flexGrow: 0 }}>
