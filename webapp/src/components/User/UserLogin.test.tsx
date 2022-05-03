c1c1d9ba42c590ce6af47bf4ea5fc675563f4503
mariaurrutia97 <uo251282@uniovi.es>
2022-05-03 15:14:43 +0200

    Code smells

--- webapp/src/components/User/UserLogin.test.tsx
+++ webapp/src/components/User/UserLogin.test.tsx
@@ -1,5 +1,4 @@
-import { fireEvent, getByRole, getByText, render, screen, waitFor, within } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
+import {getByRole, render, screen, waitFor} from '@testing-library/react';
 
 import UserLogin from './UserLogIn';
 import UserEvent from '@testing-library/user-event';
@@ -8,40 +7,38 @@ jest.mock('@inrupt/solid-client');
 
 test('check the pod inrupt provider', async () => {
 
-    render(<UserLogin />);
+  render(<UserLogin />);
 
-    UserEvent.click(getByRole(screen.getByTestId("provider"),"button"));
-    await waitFor(() => UserEvent.click(screen.getByText("https://broker.pod.inrupt.com")));
-
+  UserEvent.click(getByRole(screen.getByTestId("provider"),"button"));
+  await waitFor(() => UserEvent.click(screen.getByText("https://broker.pod.inrupt.com")));
   expect(screen.getByRole("listbox")).toHaveTextContent("https://broker.pod.inrupt.com");
-  
+ 
 });
 
 test('check the inrupt provider', async () => {
 
-    render(<UserLogin />);
+ render(<UserLogin />);
 
-    UserEvent.click(getByRole(screen.getByTestId("provider"),"button"));
-    await waitFor(() => UserEvent.click(screen.getByText("https://inrupt.net")));
-
-  expect(screen.getByRole('button',{expanded:false})).toHaveTextContent("https://inrupt.net");
+ UserEvent.click(getByRole(screen.getByTestId("provider"),"button"));
+ await waitFor(() => UserEvent.click(screen.getByText("https://inrupt.net")));
+ expect(screen.getByRole('button',{expanded:false})).toHaveTextContent("https://inrupt.net");
 
 });
 
 test('check the solid community provider', async () => {
 
-    render(<UserLogin />);
+ render(<UserLogin />);
 
-    UserEvent.click(getByRole(screen.getByTestId("provider"),"button"));
-    await waitFor(() => UserEvent.click(screen.getByText("https://solidcommunity.net/")));
+ UserEvent.click(getByRole(screen.getByTestId("provider"),"button"));
+ await waitFor(() => UserEvent.click(screen.getByText("https://solidcommunity.net/")));
+ expect(screen.getByRole('button',{expanded:false})).toHaveTextContent("https://solidcommunity.net/");
 
-  expect(screen.getByRole('button',{expanded:false})).toHaveTextContent("https://solidcommunity.net/");
 });
 
 test('check the login button', async () => {
 
-  render(<UserLogin />);
+ render(<UserLogin />);
 
-  const signIn = await screen.getByText('Sign In')
-  await waitFor(() => expect(signIn).toBeInTheDocument())
+ const signIn = await screen.getByText('Sign In')
+ await waitFor(() => expect(signIn).toBeInTheDocument())
 });
