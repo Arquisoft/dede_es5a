a2ee409d6641ce589242ef37f8faa6520351c7e1
mariaurrutia97 <uo251282@uniovi.es>
2022-05-03 12:08:13 +0200

    Test unitarios userLogin

---
+++ webapp/src/components/User/UserLogin.test.tsx
@@ -0,0 +1,47 @@
+import { fireEvent, getByRole, getByText, render, screen, waitFor, within } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+
+import UserLogin from './UserLogIn';
+import UserEvent from '@testing-library/user-event';
+
+jest.mock('@inrupt/solid-client');
+
+test('check the pod inrupt provider', async () => {
+
+    render(<UserLogin />);
+
+    UserEvent.click(getByRole(screen.getByTestId("provider"),"button"));
+    await waitFor(() => UserEvent.click(screen.getByText("https://broker.pod.inrupt.com")));
+
+  expect(screen.getByRole("listbox")).toHaveTextContent("https://broker.pod.inrupt.com");
+  
+});
+
+test('check the inrupt provider', async () => {
+
+    render(<UserLogin />);
+
+    UserEvent.click(getByRole(screen.getByTestId("provider"),"button"));
+    await waitFor(() => UserEvent.click(screen.getByText("https://inrupt.net")));
+
+  expect(screen.getByRole('button',{expanded:false})).toHaveTextContent("https://inrupt.net");
+
+});
+
+test('check the solid community provider', async () => {
+
+    render(<UserLogin />);
+
+    UserEvent.click(getByRole(screen.getByTestId("provider"),"button"));
+    await waitFor(() => UserEvent.click(screen.getByText("https://solidcommunity.net/")));
+
+  expect(screen.getByRole('button',{expanded:false})).toHaveTextContent("https://solidcommunity.net/");
+});
+
+test('check the login button', async () => {
+
+  render(<UserLogin />);
+
+  const signIn = await screen.getByText('Sign In')
+  await waitFor(() => expect(signIn).toBeInTheDocument())
+});
